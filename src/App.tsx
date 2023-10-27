828158f9fad2a46b22dd2eadc9ff251f86085e08
toannguyen <toannguyen@onefin.vn>
2023-10-27 16:08:22 +0700

    feat: custom modal with accessibility and animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Modal from "./components/Modal";
+
+import "./App.css";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const ModalBody = (
+    <>
+      <p>Content for test</p>
+      <input type="text" />
+      <input type="text" placeholder="address" />
+    </>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        <h2>Click the button to open modal!</h2>
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          aria-haspopup="true"
           rel="noopener noreferrer"
+          aria-expanded={isModalOpen}
+          onClick={(e) => openModal()}
         >
-          Learn React
-        </a>
+          Open Modal
+        </button>
+        <Modal
+          isOpen={isModalOpen}
+          title="Title"
+          content={ModalBody}
+          onClose={closeModal}
+        />
       </header>
     </div>
   );
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,138 @@
+import { FC, useEffect, useRef } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+
+import "./modal.css";
+
+interface IProps {
+  isOpen: boolean;
+  title: string | JSX.Element;
+  content: string | JSX.Element;
+  onClose: () => void;
+}
+
+type FocusElement =
+  | HTMLInputElement
+  | HTMLAnchorElement
+  | HTMLButtonElement
+  | HTMLTextAreaElement
+  | HTMLSelectElement;
+
+const Modal: FC<IProps> = ({ isOpen, title, content, onClose }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  let currentElement: FocusElement;
+
+  const handleTabKey = (e: KeyboardEvent) => {
+    if (!modalRef.current) return;
+    const focusableModalElements =
+      modalRef.current.querySelectorAll<FocusElement>(
+        'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+      );
+    const focusableModalElementsArr = Array.from(focusableModalElements);
+    const lengthFocus = focusableModalElementsArr.length;
+    if (!e.shiftKey) {
+      if (
+        focusableModalElementsArr.includes(
+          document.activeElement as FocusElement
+        )
+      ) {
+        const idxCurrent = focusableModalElementsArr.findIndex(
+          (ele) => ele === currentElement
+        );
+        if (idxCurrent > -1) {
+          if (idxCurrent + 1 < lengthFocus) {
+            const nextElement = focusableModalElementsArr[idxCurrent + 1];
+            nextElement.focus();
+            currentElement = nextElement;
+          } else {
+            focusableModalElementsArr[0].focus();
+            currentElement = focusableModalElementsArr[0];
+          }
+        }
+        return e.preventDefault();
+      } else {
+        focusableModalElementsArr[0].focus();
+        currentElement = focusableModalElementsArr[0];
+        return e.preventDefault();
+      }
+    } else {
+      if (
+        focusableModalElementsArr.includes(
+          document.activeElement as FocusElement
+        )
+      ) {
+        const idxCurrent = focusableModalElementsArr.findIndex(
+          (ele) => ele === currentElement
+        );
+        if (idxCurrent > -1) {
+          if (idxCurrent !== 0) {
+            const nextElement = focusableModalElementsArr[idxCurrent - 1];
+            nextElement.focus();
+            currentElement = nextElement;
+          } else {
+            focusableModalElementsArr[lengthFocus - 1].focus();
+            currentElement = focusableModalElementsArr[lengthFocus - 1];
+          }
+        }
+        return e.preventDefault();
+      } else {
+        focusableModalElementsArr[lengthFocus - 1].focus();
+        currentElement = focusableModalElementsArr[lengthFocus - 1];
+        return e.preventDefault();
+      }
+    }
+  };
+
+  const keyListeners = {
+    Escape: onClose,
+    Tab: handleTabKey,
+  };
+
+  useEffect(() => {
+    function keyListener(e: KeyboardEvent) {
+      const listener: any = keyListeners[e.key as keyof typeof keyListener];
+      return listener && listener(e);
+    }
+    document.addEventListener("keydown", keyListener);
+
+    return () => document.removeEventListener("keydown", keyListener);
+  }, []);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <section className="modal">
+      <div
+        ref={modalRef}
+        onClick={(e) => e.stopPropagation()}
+        className="modal-content"
+      >
+        <div className="modal-icon-exit">
+          <button className="exit-icon" onClick={onClose}>
+            <AiOutlineClose />
+          </button>
+        </div>
+        <main className="modal-main-contents">
+          <h5 className="modal-title">{title}</h5>
+          <hr />
+          <div className="modal-text">{content}</div>
+          <div className="modal-button">
+            <button
+              className="accept-btn"
+              aria-label="Close"
+              onClick={() => alert("ok")}
+            >
+              OK
+            </button>
+            <button className="close-btn" onClick={onClose}>
+              Exit
+            </button>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+};
+
+export default Modal;
